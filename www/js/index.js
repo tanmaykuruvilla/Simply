09f4ceb0becf9aa85ea77db680ae4b0fd0f73cac
pratyushajo <pjoginipally@scu.edu>
2016-03-09 10:40:58 -0800

    alert box changes

--- www/js/index.js
+++ www/js/index.js
@@ -123,7 +123,7 @@ var app = {
                     window.plugins.launcher.launch({packageName: 'com.google.android.music'}, successCallback, errorCallback);
                     break;
                 case "wifi":
-                    navigator.notification.alert("Please select WiFi from the list after hitting OK", null, "WiFi");
+                    navigator.notification.alert("Please select WiFi from the list after hitting OK", alertDismissed, "WiFi");
                     break;
                 default:
                     console.log("no support to open the application");    
